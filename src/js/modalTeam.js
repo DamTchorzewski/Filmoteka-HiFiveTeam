b2b02c1ad4aaad4bd1d99372db83bf9b75c10998
Filip Vukić <filipvukic90@gmail.com>
2023-04-22 01:32:14 +0200

    finished team-modal
    - added overflow hidden and try/catch
    - removed LI profiles
    - added email and github data
    - added minions JPGs

--- src/js/modalTeam.js
+++ src/js/modalTeam.js
@@ -1,24 +1,31 @@
 import refs from './refs';
 
 (() => {
-  window.addEventListener('click', clickBackdropCloseModal);
-  window.addEventListener('keydown', closeModalByClickEscape);
-  refs.openTeamModal.addEventListener('click', toggleModal);
-  refs.closeTeamModal.addEventListener('click', toggleModal);
+  try {
+    window.addEventListener('click', clickBackdropCloseModal);
+    window.addEventListener('keydown', closeModalByClickEscape);
+    refs.openTeamModal.addEventListener('click', toggleModal);
+    refs.closeTeamModal.addEventListener('click', toggleModal);
 
-  function clickBackdropCloseModal(e) {
-    if (e.target === refs.teamModal) {
-      refs.teamModal.classList.add('is-hidden');
+    function clickBackdropCloseModal(e) {
+      if (e.target === refs.teamModal) {
+        refs.teamModal.classList.add('is-hidden');
+      }
     }
-  }
 
-  function closeModalByClickEscape(e) {
-    if (e.code === 'Escape') {
-      refs.teamModal.classList.add('is-hidden');
+    function closeModalByClickEscape(e) {
+      if (e.code === 'Escape') {
+        refs.teamModal.classList.add('is-hidden');
+      }
     }
-  }
 
-  function toggleModal() {
-    refs.teamModal.classList.toggle('is-hidden');
+    function toggleModal() {
+      refs.teamModal.classList.toggle('is-hidden');
+      refs.body.style.overflow = 'hidden';
+      setTimeout(() => refs.openTeamModal(), 50);
+      refs.closeTeamModal(e);
+    }
+  } catch (err) {
+    console.error(err.stack);
   }
 })();
