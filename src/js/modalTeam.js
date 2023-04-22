c41736259d4e9d04df3136b1dff3a0c602d2ec22
Beniamin Brzeziński <benek9311@wp.pl>
2023-04-22 11:47:09 +0200

    Poprawki

--- src/js/modalTeam.js
+++ src/js/modalTeam.js
@@ -1,35 +1,31 @@
 import refs from './refs';
 
 (() => {
-  try {
-    window.addEventListener('click', clickBackdropCloseModal);
-    refs.openTeamModal.addEventListener('click', toggleModal);
-    refs.closeTeamModal.addEventListener('click', () => {
-      toggleModal();
+  window.addEventListener('click', clickBackdropCloseModal);
+  refs.openTeamModal.addEventListener('click', toggleModal);
+  refs.closeTeamModal.addEventListener('click', () => {
+    toggleModal();
+    refs.body.style.overflow = 'unset';
+  });
+
+  function clickBackdropCloseModal(e) {
+    if (e.target === refs.teamModal) {
+      refs.teamModal.classList.add('is-hidden');
       refs.body.style.overflow = 'unset';
-    });
-
-    function clickBackdropCloseModal(e) {
-      if (e.target === refs.teamModal) {
-        refs.teamModal.classList.add('is-hidden');
-        refs.body.style.overflow = 'unset';
-      }
     }
+  }
 
-    function closeModalByClickEscape(e) {
-      if (e.code === 'Escape') {
-        refs.teamModal.classList.add('is-hidden');
-        refs.body.style.overflow = 'unset';
-        window.removeEventListener('keydown', closeModalByClickEscape);
-      }
+  function closeModalByClickEscape(e) {
+    if (e.code === 'Escape') {
+      refs.teamModal.classList.add('is-hidden');
+      refs.body.style.overflow = 'unset';
+      window.removeEventListener('keydown', closeModalByClickEscape);
     }
+  }
 
-    function toggleModal() {
-      refs.teamModal.classList.toggle('is-hidden');
-      refs.body.style.overflow = 'hidden';
-      window.addEventListener('keydown', closeModalByClickEscape);
-    }
-  } catch (err) {
-    console.error(err.stack);
+  function toggleModal() {
+    refs.teamModal.classList.toggle('is-hidden');
+    refs.body.style.overflow = 'hidden';
+    window.addEventListener('keydown', closeModalByClickEscape);
   }
 })();
