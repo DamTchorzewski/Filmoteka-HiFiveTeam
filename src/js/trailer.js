4caac572ce80de30da3f600d03cf516c95f8caa5
DagmaraSobczak <daga.sobczak@gmail.com>
2023-04-23 21:54:36 +0200

    poprawka

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -1,8 +1,9 @@
 import Api from './api';
 import * as basicLightbox from 'basiclightbox';
+import { Notify } from 'notiflix';
+import refs from './refs';
 
-const playButton = document.querySelector('.modal__btn-trailer');
-playButton.addEventListener('click', e => {
+refs.watchTrailer.addEventListener('click', e => {
   const movieId = e.currentTarget.dataset.id;
   Api.getMovieTrailer(movieId)
     .then(data => {
@@ -10,14 +11,28 @@ playButton.addEventListener('click', e => {
 
       if (trailer) {
         const instance = basicLightbox.create(`
-    <iframe src="https://www.youtube.com/watch?v=${trailer.key}&autoplay=1"
+    <iframe src="https://www.youtube.com/embed/${trailer.key}?autoplay=1"
      width="560"
       height="315"
       frameborder="0"></iframe>
 `);
-        instance.show();
+        const closeOnEsc = ({ code }) => {
+          if (code === 'Escape') {
+            instance.close(window.removeEventListener('keydown', closeOnEsc));
+            refs.watchTrailer.classList.toggle('active-btn');
+          }
+        };
+
+        instance.show(window.addEventListener('keydown', closeOnEsc));
+
+        refs.watchTrailer.classList.toggle('active-btn');
       } else {
         console.error('Nie znaleziono zwiastunu');
+        Notify.failure('Ops, trailer was not found!', {
+          position: 'center-center',
+          timeout: 3500,
+          pauseOnHover: false,
+        });
       }
     })
     .catch(error => {
