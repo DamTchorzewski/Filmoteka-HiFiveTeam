05abc3ab8cdc745c2c5622ea36e8155268cd0c81
DagmaraSobczak <daga.sobczak@gmail.com>
2023-04-23 09:31:09 +0200

    Create trailer.js

---
+++ src/js/trailer.js
@@ -0,0 +1,25 @@
+import Api from './api';
+import * as basicLightbox from 'basiclightbox';
+
+const playButton = document.querySelector('.trailerButton'); //potrzeba stworzyć button do karty filmu, który będzie uruchamiał trailer
+playButton.addEventListener('click', () => {
+  Api.getMovieTrailer()
+    .then(data => {
+      const trailer = data.results.find(result => result.type === 'Trailer');
+
+      if (trailer) {
+        const instance = basicLightbox.create(`
+    <iframe src="https://www.youtube.com/watch?v=${trailer.key}&autoplay=1"
+     width="560"
+      height="315"
+      frameborder="0"></iframe>
+`);
+      } else {
+        console.error('Nie znaleziono zwiastunu');
+      }
+    })
+    .catch(error => {
+      console.error('Wystąpił błąd:', error);
+    });
+});
+instance.show();
